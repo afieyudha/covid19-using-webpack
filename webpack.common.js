5164927ac26cb8e0019a8b0d0447556e4cdd0073
Afie Yudha Triadi <ayudhatriadi20@gmail.com>
2020-04-28 14:10:30 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,9 @@ module.exports = {
                 use: {
 					loader: 'babel-loader',
 				},
+		              		options: {
+				    presets: ["@babel/preset-env", "@babel/preset-react"],
+				  },
             },
             {
                 test: /\.(scss|sass)$/i,
@@ -32,11 +35,8 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                  // Creates `style` nodes from JS strings
                   'style-loader',
-                  // Translates CSS into CommonJS
                   'css-loader',
-                  // Compiles Sass to CSS
                   'sass-loader',
                 ],
             },
